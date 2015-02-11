cb9b9a81deee587be6c837b7ff16254f339e1282
Tatsunori TAMAKI <github@qtamaki.com>
2015-02-11 15:55:16 +0900

    自然数0..120のリストをランダムに並べ、100個を取る処理をリファクタ(by #rpscala)

--- src/test/scala/examples/ss1/SmallestFreeNumberSpec.scala
+++ src/test/scala/examples/ss1/SmallestFreeNumberSpec.scala
@@ -1,7 +1,7 @@
 package examples.ss1
 
 import org.scalatest._
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.{BooleanOperators, forAll}
 import org.scalacheck.Gen
 import scala.util.Random
 import scala.collection.mutable.HashSet
@@ -21,7 +21,10 @@ class SmallestFreeNumberSpec extends FlatSpec with Matchers {
     } f(list) should be (ans)
   }
   it should "Check" in {
-    val gen = Gen.resultOf { x: Int =>
+    val gen = Gen.resultOf { _: Int =>
+    Random.shuffle(0 until 120 toList).take(100)
+    /*
+    (0 until 120).toList.sortBy(_ => Random.nextDouble()).take(100)
       var set = new HashSet[Int]()
       def fun: Int = {
         val x = Random.nextInt(120)
@@ -33,16 +36,17 @@ class SmallestFreeNumberSpec extends FlatSpec with Matchers {
         }
       }
       List.fill(100)(0).map(x => fun)
+      */
     }
     def makeProp(f: List[Int] => Int) = forAll(gen) { (xs: List[Int]) =>
       val ans1 = f(xs)
       Stream.from(0).zip(xs.sorted).filter(x => x._1 != x._2) match {
-        case Stream.Empty => true
+        case Stream.Empty => true :| "ok"
         case noneEmpty =>
           val ans2 = noneEmpty.head._1
           ans1 should be <= xs.max
           ans1 should be(ans2)
-          ans1 == ans2
+          (ans1 == ans2) :| s"NGNG!! ${ans1} == ${ans2}"
       }
     }
     fs.foreach {f => makeProp(f).check}
