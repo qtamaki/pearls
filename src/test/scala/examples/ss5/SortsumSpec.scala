60ccf2a48cb60f35098ead162e29aed9a8d4077c
Tatsunori TAMAKI <github@qtamaki.com>
2015-03-05 10:40:13 +0900

    テストケース追加

--- src/test/scala/examples/ss5/SortsumSpec.scala
+++ src/test/scala/examples/ss5/SortsumSpec.scala
@@ -4,6 +4,9 @@ import org.scalatest._
 
 class SortsumSpec extends FlatSpec with Matchers {
   it should "Sortsum function 1" in {
-    Sortsum.sortsum(List(1,2,3), List(4,5,6))
+    Sortsum.sortsum(List(1,2,3), List(4,5,6)) should be (List(5, 6, 6, 7, 7, 7, 8, 8, 9))
+  }
+  it should "Sortsum function 2" in {
+    Sortsum2.sortsums(List(1,2,3), List(4,5,6)) should be (List(5, 6, 6, 7, 7, 7, 8, 8, 9))
   }
 }
